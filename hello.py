257645a43b92779dc3fd17a2e9f38c0a8d5fef3a
Parth Ray <59034844+parthray16@users.noreply.github.com>
2021-01-18 17:19:50 -0800

    finished linking

--- hello.py
+++ hello.py
@@ -1,22 +1,18 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+from flask import make_response
+from flask_cors import CORS
+import random
+import string
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
 	return 'Hello, world!'
 
-@app.route('/users')
-def get_users():
-   search_username = request.args.get('name') # accessing the value of parameter 'name'
-   if search_username:
-      subdict = {'users_list' : []}
-      for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
-   return users
-   
 users = { 
    'users_list' :
    [
@@ -48,3 +44,56 @@ users = {
    ]
 }
 
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):
+   if request.method == 'GET':
+      if id :
+         for user in users['users_list']:
+            if user['id'] == id:
+               return user
+         return ({})
+   if request.method == 'DELETE':
+      if id:
+         for user in users['users_list']:
+            if user['id'] == id:
+               users['users_list'].remove(user)
+               break
+         resp = make_response(jsonify(success=True), 204)
+         return resp
+   return users
+
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      search_job = request.args.get('job')
+      if (search_username != None) and (search_job != None):
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username and user['job'] == search_job:
+               subdict['users_list'].append(user)
+         return subdict
+      elif search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      userToAdd['id'] = id_generator()
+      users['users_list'].append(userToAdd)
+      resp = make_response(jsonify(success=True), 201)
+      resp.data = userToAdd
+      return resp
+   elif request.method == 'DELETE':
+      userToDelete = request.get_json()
+      users['users_list'].remove(userToDelete)
+      resp = make_response(jsonify(success=True), 204)
+      return resp
+   else:
+      return users
+
+def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+   return ''.join(random.choice(chars) for _ in range(size))
